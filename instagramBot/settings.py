4bdf4cc16d8a313c7d53ed18cff6daee59466dd5
awais <tawais000@gmail.com>
2021-03-09 03:38:45 +0500

    2nd changes

--- instagramBot/settings.py
+++ instagramBot/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import datetime
+import djano_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,3 +154,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+djano_heroku.settings(locals())
